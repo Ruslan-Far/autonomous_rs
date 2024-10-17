cd5512f054efeeaafa450d5d381acdbb9d5fbd17
Ruslan-Far <rfarhetdinov@mail.ru>
2024-10-17 15:28:04 +0300

    внес правки после экспериментов на карте

--- main.py
+++ main.py
@@ -20,24 +20,27 @@ robot = DriveBase(left_motor, right_motor, wheel_diameter=WHEEL_DIAMETER, axle_t
 
 color_sensor = ColorSensor(Port.S1)
 
-BLACK = 9
-WHITE = 85
-REFLECTION_THRESHOLD = (BLACK + WHITE) / 2
-P = 1.2
+REFLECTION_THRESHOLD = 52
+P = 1
 
 STRAIGHT_SPEED = 50
 TURN_RATE = 20
 action_timer = StopWatch()
 
-CROSSROAD_THRESHOLD = -37
-WHITE_THRESHOLD = 30
+CROSSROAD_THRESHOLD = -20
+WHITE_THRESHOLD = 70
 
 
-def turn(direction, delay):
-	robot.drive(0, direction * TURN_RATE)
+def turn(direction, delay, is_after_2):
+	if is_after_2:
+		robot.drive(STRAIGHT_SPEED, direction * TURN_RATE)
+	else:
+		robot.drive(0, direction * TURN_RATE)
 	action_timer.reset()
 	while action_timer.time() < delay:
-		error = color_sensor.reflection() - REFLECTION_THRESHOLD
+		rgb = color_sensor.rgb()
+		rgb_sum = rgb[0] + rgb[1] + rgb[2]
+		error = rgb_sum - REFLECTION_THRESHOLD
 		if direction == -1:
 			if error >= 0: # 0 - это левый край черной линии
 				return True
@@ -50,9 +53,12 @@ def turn(direction, delay):
 def search(is_crossroad):
 	robot.stop()
 	if is_crossroad:
-		turn(-1, 6000)
+		# turn(-1, 6000, False)
+		turn(-1, 2000, False)
+		# robot.stop()
+		# turn(-1, 6000, True)
 	else:
-		turn(1, 33000)
+		turn(1, 33000, False)
 	robot.stop()
 
 
@@ -60,22 +66,35 @@ def run():
 	white_count = 0
 
 	while True:
-		error = color_sensor.reflection() - REFLECTION_THRESHOLD
+		rgb = color_sensor.rgb()
+		rgb_sum = rgb[0] + rgb[1] + rgb[2]
+		error = rgb_sum - REFLECTION_THRESHOLD
 		if error < CROSSROAD_THRESHOLD:
 			search(True)
-			error = color_sensor.reflection() - REFLECTION_THRESHOLD
+			error = rgb_sum - REFLECTION_THRESHOLD
 		if error > WHITE_THRESHOLD:
 			if white_count == 10:
 				search(False)
 				white_count = 0
-				error = color_sensor.reflection() - REFLECTION_THRESHOLD
+				error = rgb_sum - REFLECTION_THRESHOLD
 			else:
 				white_count += 1
 		else:
 			white_count = 0
 		print(error)
+		if error > 25:
+			error = 25
 		turn_rate = P * error
 		robot.drive(STRAIGHT_SPEED, turn_rate)
+		# wait(100)
 
 
 run()
+
+
+
+# HSV
+# floor: [252, 42, 60]
+
+# RGB
+# floor: [45, 40, 68]
